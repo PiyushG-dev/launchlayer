89fdb6d6c3605d1580e50eb314081f2a74362908
Piyush Goswami <pjgpiyush@gmail.com>
2024-11-09 15:32:36 +0530

    customers and features pages updated

--- app/customers/layout.js
+++ app/customers/layout.js
@@ -2,23 +2,29 @@ import HeroHeading from "@/components/ui/hero-heading";
 import PageLinks from "@/components/ui/page-links";
 import { designLinks } from "@/constants";
 import { Switch } from "@/components/ui/switch";
-export default function CustomersLayout({ children }) {
+export default function GuidesLayout({ children }) {
   return (
-    <div>
-      <HeroHeading title="SaaS design" label="Design inspirations" emoji="🚀" />
-      <div className="max-w-screen-xl mx-auto flex flex-col items-center gap-6">
-        <PageLinks pageLinks={designLinks} />
-        <div className="flex items-center gap-2">
-          <p className="text-sm text-slate-950 font-medium tracking-tight">
-            Light themes
-          </p>
-          <Switch />
-          <p className="text-sm text-slate-950 font-medium tracking-tight">
-            Dark themes
-          </p>
+    <div className="w-full">
+      <div className="max-w-screen-xl mx-auto px-6">
+        <HeroHeading
+          title="SaaS design"
+          label="Design inspirations"
+          emoji="🚀"
+        />
+        <div className="flex flex-col items-center gap-6">
+          <PageLinks pageLinks={designLinks} />
+          <div className="flex items-center gap-2">
+            <p className="text-sm text-slate-950 font-medium tracking-tight">
+              Light themes
+            </p>
+            <Switch />
+            <p className="text-sm text-slate-950 font-medium tracking-tight">
+              Dark themes
+            </p>
+          </div>
         </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 }
--- app/features/layout.js
+++ app/features/layout.js
@@ -2,23 +2,30 @@ import HeroHeading from "@/components/ui/hero-heading";
 import PageLinks from "@/components/ui/page-links";
 import { designLinks } from "@/constants";
 import { Switch } from "@/components/ui/switch";
+import DesignBlockWrapper from "@/components/ui/design-block-wrapper";
 export default function FeaturesLayout({ children }) {
   return (
-    <div>
-      <HeroHeading title="SaaS design" label="Design inspirations" emoji="🚀" />
-      <div className="max-w-screen-xl mx-auto flex flex-col items-center gap-6">
-        <PageLinks pageLinks={designLinks} />
-        <div className="flex items-center gap-2">
-          <p className="text-sm text-slate-950 font-medium tracking-tight">
-            Light themes
-          </p>
-          <Switch />
-          <p className="text-sm text-slate-950 font-medium tracking-tight">
-            Dark themes
-          </p>
+    <div className="w-full">
+      <div className="max-w-screen-xl mx-auto px-6">
+        <HeroHeading
+          title="SaaS design"
+          label="Design inspirations"
+          emoji="🚀"
+        />
+        <div className="flex flex-col items-center gap-6">
+          <PageLinks pageLinks={designLinks} />
+          <div className="flex items-center gap-2">
+            <p className="text-sm text-slate-950 font-medium tracking-tight">
+              Light themes
+            </p>
+            <Switch />
+            <p className="text-sm text-slate-950 font-medium tracking-tight">
+              Dark themes
+            </p>
+          </div>
         </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 }
