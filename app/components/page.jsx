d9c87e2b790e5502b2099141944f699c6c0940a1
Piyush Goswami <pjgpiyush@gmail.com>
2024-12-24 18:20:20 +0530

    heroheading comp updated

--- app/components/page.jsx
+++ app/components/page.jsx
@@ -2,30 +2,6 @@ import PackageCmdCopy from "@/components/ui/package-cmd-copy";
 import React from "react";
 import { componentPages } from "@/constants";
 import ComponentPageCard from "@/components/ui/component-page-card";
-// import ComponentViewer from "@/components/layout/ComponentViewer";
-// import SampleComponent from "@/components/user-library/SampleComponent";
-
-// const sampleCode = `
-// import React from "react";
-
-// const SampleComponent = () => {
-//   return (
-//     <div className="text-center p-6 bg-gradient-to-r from-purple-400 to-blue-500 text-white rounded-lg shadow-md">
-//       <h1 className="text-2xl font-bold">Data to Enrich Your Online Business</h1>
-//       <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//       <button className="mt-4 px-6 py-2 bg-white text-blue-500 font-bold rounded-md">
-//         Get Started
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SampleComponent;
-// `;
-
-// const Components = () => {
-//   return <ComponentViewer component={SampleComponent} code={sampleCode} />;
-// };
 
 const Components = () => {
   return (
--- components/ui/hero-heading.jsx
+++ components/ui/hero-heading.jsx
@@ -17,17 +17,18 @@ const HeroHeading = () => {
                   `mr-1 inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
                 )}
               >
-                Powered by shadcn/ui
+                New blocks every week
               </span>
               <span>🚀</span>
             </AnimatedGradientText>
-            <h1 className="text-center text-5xl tracking-tighter sm:text-7xl sm:tracking-[-3.4px] sm:max-w-3xl mt-2">
-              An extensive collection of shadcn blocks
+            <h1 className="text-center text-5xl tracking-tighter md:text-8xl sm:text-7xl md:tracking-[-4.8px] md:max-w-[62rem] mt-2">
+              Making design easy for engineers
             </h1>
           </div>
           <p className="tracking-tight sm:text-lg text-muted-foreground sm:tracking-tighter sm:w-3/4 lg:w-2/4 text-center">
-            Open source components built with React, JavaScript, Tailwind CSS,
-            and Framer Motion &#8208; the perfect companion for shadcn/ui.
+            An extensive library of beautifully crafted, open-source component
+            blocks built with the versatile and reusable components of
+            shadcn/ui.
           </p>
           <div className="flex flex-col sm:flex-row items-center gap-3">
             <Button asChild>
